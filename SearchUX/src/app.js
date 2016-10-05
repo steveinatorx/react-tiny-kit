86213e6a38182ee0ec95d03cda6c6d45869a271d
steveinatorx <steveinatorx@gmail.com>
2016-10-05 13:48:08 -0700

    cleanup app.js

--- SearchUX/src/app.js
+++ SearchUX/src/app.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
- import { createStore } from 'redux';
- import { Provider } from 'react-redux';
- import  reducer  from './reducer';
- import { SearchUXContainer } from './containers';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+import { SearchUXContainer } from './containers';
  const store = createStore(reducer);
  console.log('alive1');
 
 render(
-  <Provider store = {store} >
+  <Provider store={store} >
     <SearchUXContainer />
   </Provider>,
   document.getElementById('app')
