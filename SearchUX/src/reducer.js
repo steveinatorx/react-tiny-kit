b2e2b9a9b5e8c2cede5f9d255ee4c9bfd0d79938
steveinatorx <steveinatorx@gmail.com>
2016-10-05 13:49:12 -0700

    fix updater format

--- SearchUX/src/reducer.js
+++ SearchUX/src/reducer.js
@@ -1,22 +1,24 @@
 import { List, Map } from 'immutable';
 
 const init = List([
-  Map({ id: 'Model', isActive: true }),
-  Map({ id: 'Body', isActive: true }),
-  Map({ id: 'Year', isActive: true }),
+  Map({ id: 'Model', isActive: false }),
+  Map({ id: 'Body', isActive: false }),
+  Map({ id: 'Year', isActive: false }),
 ]);
 
 export default function reducer (activeFields = init, action) {
+  console.log('in reducer', action.payload);
   switch (action.type) {
     case 'SET_ACTIVE_FIELD':
       return activeFields.map(f => {
+        // console.log('mapiing reducer->', f.get('id'));
         if(f.get('id') === action.payload) {
-          return f.update('isActive', true);
+          return f.update('isActive', isActive => true);
         } else {
-          return f;
+          return f.update('isActive', isActive => false);
         }
       });
     default:
       return activeFields;
   }
-}
+};
