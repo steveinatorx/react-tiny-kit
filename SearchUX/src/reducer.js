52f5922cdb6405dacb58c3661ebfa532e3911198
steveinatorx <steveinatorx@gmail.com>
2016-10-04 22:53:06 -0700

    working manual field Map []

--- SearchUX/src/reducer.js
+++ SearchUX/src/reducer.js
@@ -1,9 +1,13 @@
 import { List, Map } from 'immutable';
 
-const init = List([]);
+const init = List([
+  Map({ id: 'Model', isActive: true }),
+  Map({ id: 'Body', isActive: true }),
+  Map({ id: 'Year', isActive: true }),
+]);
 
-export default function reducer(activeFields=init, action) {
-  switch(action.type) {
+export default function reducer (activeFields = init, action) {
+  switch (action.type) {
     case 'SET_ACTIVE_FIELD':
       return activeFields.map(f => {
         if(f.get('id') === action.payload) {
