b120aa5de389a5aa4ffc439f8b5ce176c1f4db7c
steveinatorx <steveinatorx@gmail.com>
2016-10-13 13:56:35 -0700

    add react-router-redux to manage url

--- SearchUX/src/app.js
+++ SearchUX/src/app.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './reducer';
+import reducer from './reducers/reducers';
+import routing from './reducers/routing';
 import { SearchUXContainer } from './containers';
+
+import { syncHistoryWithStore } from 'react-router-redux';
+import { createHistory } from 'history';
+//import routing from 'reducers/routing';
+
  const store = createStore(reducer);
  console.log('alive1');
 
