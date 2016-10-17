1bca56509da9741f06d5f57820e766795f6a530c
steveinatorx <steveinatorx@gmail.com>
2016-10-17 12:36:36 -0700

    cache model vals

--- SearchUX/src/reducers/reducers.js
+++ SearchUX/src/reducers/reducers.js
@@ -3,7 +3,22 @@ import { LOCATION_CHANGE } from 'react-router-redux';
 
 //can i just render the same component with different opts fed by this Map?
 const init = List([
-  Map({ id: 'Model', idx: 0, isActive: true, opts: [], multi: false }),
+  Map({ id: 'Model', idx: 0, isActive: true, opts: [
+	{ label: "980 Carrera GT", value: "980 Carrera GT"},
+	{ label: "GT3 3.6", value: "GT3 3.6"},
+	{ label: "GT3 RS 3.6", value: "GT3 RS 3.6" },
+	{ label: "GT3 3.8", value: "GT3 3.8" },
+	{ label: "GT3 RS 3.8", value: "GT3 RS 3.8" },
+	{ label: "Turbo 3.6", value: "Turbo 3.6" },
+	{ label: "Turbo 3.8", value: "Turbo 3.8" },
+	{ label: "Turbo S 3.8", value: "Turbo S 3.8" },
+	{ label: "997 Speedster", value: "997 Sppedster" },
+	{ label: "GT2", value: "GT2" },
+	{ label: "918 Spyder", value: "918 Spyder" },
+ 	{ label: "GT2", value: "GT2"}, 
+ 	{ label: "GT2 RS", value: "GT2 RS"}, 
+	{ label: "GT3 RS 4.0", value: "GT3 RS 4.0" },
+], multi: false }),
   Map({ id: 'Body', idx: 1, isActive: false, opts: [], multi: true }),
   Map({ id: 'Year', idx: 2, isActive: false, Opts: [], multi: true }),
 ]);
