f8d1e225efe97d2ddd7dc2c922980fd86f41631b
steveinatorx <steveinatorx@gmail.com>
2016-10-17 12:49:37 -0700

    clean up view

--- SearchUX/src/components.js
+++ SearchUX/src/components.js
@@ -7,20 +7,6 @@ import Select from 'react-select';
 
 import './css/react-select.css';
 
-
-const FLAVOURS = [
-	{ label: 'Chocolate', value: 'chocolate' },
-	{ label: 'Vanilla', value: 'vanilla' },
-	{ label: 'Strawberry', value: 'strawberry' },
-	{ label: 'Caramel', value: 'caramel' },
-	{ label: 'Cookies and Cream', value: 'cookiescream' },
-	{ label: 'Peppermint', value: 'peppermint' },
-];
-
-const WHY_WOULD_YOU = [
-	{ label: 'Chocolate (are you crazy?)', value: 'chocolate', disabled: true },
-].concat(FLAVOURS.slice(1));
-
 var MultiSelectField = React.createClass({
 	displayName: 'MultiSelectField',
 	propTypes: {
@@ -31,7 +17,6 @@ var MultiSelectField = React.createClass({
     console.log(' in gIS MSF:', this.props);
 		return {
 			disabled: false,
-			crazy: false,
 			options: this.props.activeField.opts,
 			value: [],
 		};
@@ -49,30 +34,19 @@ var MultiSelectField = React.createClass({
 	toggleDisabled (e) {
 		this.setState({ disabled: e.target.checked });
 	},
+  /*
 	toggleChocolate (e) {
 		let crazy = e.target.checked;
 		this.setState({
 			crazy: crazy,
 			options: crazy ? WHY_WOULD_YOU : FLAVOURS,
 		});
-	},
+	},*/
 	render () {
 		return (
 			<div className="section">
 				<h3 className="section-heading">{this.props.label}</h3>
-        foobar
 				<Select multi simpleValue disabled={this.state.disabled} value={this.state.value} placeholder="Select your favourite(s)" options={this.state.options} onChange={this.handleSelectChange} />
-
-				<div className="checkbox-list">
-					<label className="checkbox">
-						<input type="checkbox" className="checkbox-control" checked={this.state.disabled} onChange={this.toggleDisabled} />
-						<span className="checkbox-label">Disable the control</span>
-					</label>
-					<label className="checkbox">
-						<input type="checkbox" className="checkbox-control" checked={this.state.crazy} onChange={this.toggleChocolate} />
-						<span className="checkbox-label">I don't like Chocolate (disabled the option)</span>
-					</label>
-				</div>
 			</div>
 		);
 	}
@@ -218,26 +192,29 @@ export default class SearchListNav extends React.Component {
   }
 
   render() {
-    return (
-      <div className="container" onKeyDown={this.handleKeyDown}>
-        <ol className="progtrckr">
-          {this.renderSteps()}
-        </ol>
-        {/* render component via var name? */}
-        {/* this.props.steps[this.state.compState].component */}
-        <span>{ this.state.compState } </span>
-          <MultiSelectField {...this.props} activeField={this.state.activeSearchField} />
-        <p></p>
-        {/* <div style={this.props.showNavigation ? {} : this.hidden}> */}
-        <div>
-          <button style={this.state.showPreviousBtn ? {} : this.hidden}
-                  className="multistep__btn--prev"
-                  onClick={this.previous}>Previous</button>
+      return (
+        <div className="container" onKeyDown={this.handleKeyDown}>
+          <ol className="progtrckr">
+            {this.renderSteps()}
+          </ol>
+          {/* render component via var name? */}
+          {/* this.props.steps[this.state.compState].component */}
+          {/* this.state.compState */} 
+            <div className="row">
+              <div className="six columns">
+                <MultiSelectField {...this.props} activeField={this.state.activeSearchField} />
+                  {/* <div style={this.props.showNavigation ? {} : this.hidden}> */}
+                  <div>
+                    <button style={this.state.showPreviousBtn ? {} : this.hidden}
+                            className="multistep__btn--prev"
+                            onClick={this.previous}>Previous</button>
 
-          <button style={this.state.showNextBtn ? {} : this.hidden}
-                  className="multistep__btn--next"
-                  onClick={this.next}>Next</button>
-        </div>
+                    <button style={this.state.showNextBtn ? {} : this.hidden}
+                            className="multistep__btn--next"
+                            onClick={this.next}>Next</button>
+                  </div>
+              </div>
+            </div>
       </div>
     );
   }
