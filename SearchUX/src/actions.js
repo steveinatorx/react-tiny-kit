21f2bc4ad8795b3dfad7e385a042beb7ac4c6cac
steveinatorx <steveinatorx@gmail.com>
2016-11-03 13:59:01 -0700

    add composite thunk

--- SearchUX/src/actions.js
+++ SearchUX/src/actions.js
@@ -11,20 +11,101 @@ export function setActiveField(idx) {
   };
 }
 
-export const SET_FIELD_SELECTION = 'SET_FIELD_SELECTION';
 
+export const SET_FIELD_SELECTION = 'SET_FIELD_SELECTION';
 export function setFieldSelection(idx,selection) {
-  return {
+  return dispatch => {
+    dispatch({
     type: 'SET_FIELD_SELECTION',
     payload: {
       idx: idx,
       selection: selection
     }
-  };
+    });
+  }
 }
 
-export const REQUEST_FIELDS = 'REQUEST_FIELDS'
 
+
+var buildQueryObj = function buildQueryObj(state){
+    console.log('in buildQ actions', state.reducer);
+
+ var queryRoot = {};
+          state.reducer.getIn(['searchFields']).map(f => {
+          console.log('i&&^&^&^&^&^^&^& from selected', f.get('selected'));
+          console.log('i&&^&^&^&^&^^&^& ujsingField', f.get('id'));
+          var theValue = f.get('selected');
+          var theField = f.get('id');
+          
+          if( theField !== 'Opts' && theValue.length > 0) {
+             //coding around that fucking react-select API inconsistency.....
+             if (theValue[0].match(/,/)){
+               theValue = theValue[0].split(',');
+               theValue=theValue.splice(0,theValue.length - 1);
+              }
+
+            if( theValue.length > 1) {
+               //  console.log('mutli selection');
+                var theObj = {};
+                theObj.$in=theValue;
+                queryRoot[f.get('id')]=theObj;
+              }
+              else {
+                if (f.get('id') === 'Year') {
+                  queryRoot[f.get('id')] = parseInt(theValue);
+                } else {
+                  queryRoot[f.get('id')] = theValue;
+                }
+              }
+      } else if ( theValue.length > 0 && theField ==='Opts'){
+        console.log('detected opts', theValue);
+            if (theValue[0].match(/,/)){
+                //trim damn API inconsistency
+
+               var theObj={};
+               var theValueArr = theValue[0].split(',');
+               console.log('theValueArr', theValueArr);
+               theObj.$in=theValueArr;
+               queryRoot[theField] = theObj;  
+            } else
+            {
+                var theObj = {};
+                theObj.$in=theValue;
+                queryRoot[theField] = theObj;  
+            }   
+
+      }
+      
+      }) 
+
+      return queryRoot;
+}
+
+export function setFieldSelectionAndFetchData(idx,selection){
+  return function (dispatch, getState) {
+    dispatch(setFieldSelection(idx,selection));
+    var state=getState();
+    var qObj = buildQueryObj(state);
+    //only get new count on Opts selection
+    var nextFieldMap = state.reducer.getIn(['searchFields',idx+1]);
+    if (idx!=7){
+      var nextId = nextFieldMap.get('id');
+      console.log('now fetch fields for ->', nextId);
+      console.log('using ->', qObj);
+      dispatch(fetchFields(nextId, qObj));
+    } else {
+      console.log('using ->', qObj);
+      dispatch(fetchCount(qObj))      
+    }
+  } 
+}//endfunc
+
+
+
+
+
+
+export const REQUEST_FIELDS = 'REQUEST_FIELDS'
 export function requestFields(field) {
   return {
     type: REQUEST_FIELDS,
