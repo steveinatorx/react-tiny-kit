9aa1a4cf5071f67ccb13d31e83b8b7277488b23b
steveinatorx <steveinatorx@gmail.com>
2016-11-03 18:41:14 -0700

    add action to prop

--- SearchUX/src/containers.js
+++ SearchUX/src/containers.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import  SearchListNav  from './components';
-import { setActiveField, setFieldSelection, setFieldSelectionAndFetchData, fetchFields, fetchCount } from './actions';
+import { setActiveField, setFieldSelection, clearAll, setFieldSelectionAndFetchData, fetchFields, fetchCount } from './actions';
 
 export const SearchUXContainer = connect(
   /*componentDidMount: function() {
@@ -25,6 +25,7 @@ function mapStateToProps (state) {
       setFieldSelectionAndFetchData: (idx, selection) => dispatch(setFieldSelectionAndFetchData(idx,selection)),
       fetchFields: (idx, queryObj) => dispatch(fetchFields(idx, queryObj)),
       fetchCount: (queryObj) => dispatch(fetchCount(queryObj)),
+      clearAll: () => dispatch(clearAll()),
     };
   }
 )(SearchListNav);
