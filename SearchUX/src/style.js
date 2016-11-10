65b30332ec0b8cc67b81e5d0ebc04097db6a3c08
steveinatorx <steveinatorx@gmail.com>
2016-11-10 11:01:53 -0800

    separate file for inline style objs

---
+++ SearchUX/src/style.js
@@ -0,0 +1,27 @@
+export default {
+	breakpointLarge: '@media screen and (min-width: 64em)',
+  	breakpointSmall: '@media screen and (max-width: 20em)',
+    resultsBox:  {
+      fontSize: '1.5em',
+      fontWeight: 'bold',
+      borderRadius: '5px',
+      borderWidth: '1px',
+      borderColor: '#4cae4c',
+      borderStyle: 'solid',
+      margin: '10px',
+      padding: '10px',
+      color: 'white',
+    },
+	resultsBoxGreen: {
+      backgroundColor : '#5cb85c',
+	},
+	resultsBoxRed: {
+	  backgroundColor: 'red'
+	},
+    hidden: { 
+     visibility: 'hidden' 
+    },
+	selectionTd: {
+	 color: 'silver'	
+	}
+};
