83f785254de06748b215baa052591251046cd5f5
steveinatorx <steveinatorx@gmail.com>
2016-11-12 14:17:35 -0800

    conditional clevertap code

--- SearchUX/src/app.js
+++ SearchUX/src/app.js
@@ -9,6 +9,8 @@ import createHistory from 'history/createBrowserHistory'
 import { routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import {browserHistory} from 'react-router';
+var __CONFIG__ = require('__CONFIG__');
+
 /*import { createDevTools } from 'redux-devtools'
 import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
@@ -18,6 +20,20 @@ export const DevTools = createDevTools(
     <LogMonitor theme="tomorrow" preserveScrollTop={false} />
   </DockMonitor>
 )*/
+
+
+if (__CONFIG__.thisEnv === 'test') {
+ var clevertap = {event:[], profile:[], account:[], onUserLogin:[], notifications:[]};
+ clevertap.account.push({"id": __CONFIG__.clevertap});
+ (function () {
+		 var wzrk = document.createElement('script');
+		 wzrk.type = 'text/javascript';
+		 wzrk.async = true;
+		 wzrk.src = ('https:' == document.location.protocol ? 'https://d2r1yp2w7bby2u.cloudfront.net' : 'http://static.clevertap.com') + '/js/a.js';
+		 var s = document.getElementsByTagName('script')[0];
+		 s.parentNode.insertBefore(wzrk, s);
+  })();
+}
 
 
 const rootReducer = combineReducers( { reducer, routing: routerReducer} );  
