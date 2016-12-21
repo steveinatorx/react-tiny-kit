4a97aa106bfefbb0d99a9b6af8ab17d88ec6f54e
steveinatorx <steveinatorx@gmail.com>
2016-12-20 20:34:02 -0800

    readd routing

--- SearchUX/src/reducers/routing.js
+++ SearchUX/src/reducers/routing.js
@@ -1,9 +1,8 @@
 import { LOCATION_CHANGE } from 'react-router-redux';
-import { SET_ACTIVE_FIELD } from '../actions';
 // This initial state is *copied* from react-router-redux's
 // routerReducer (the property name 'locationBeforeTransitions' is
 // because this is designed for use with react-router)
-const initialState = { locationBeforeTransitions: null };
+const initialState = new Map({ locationBeforeTransitions: null });
 
 export default function routerReducer(state = initialState, action) {
   // This LOCATION_CHANGE case is copied from react-router-redux's routerReducer
