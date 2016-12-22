61447567c584270a86639171f9da8c7243df1940
steveinatorx <steveinatorx@gmail.com>
2016-12-22 13:01:10 -0800

    reduce to eliminate bug

--- SearchUX/src/root.js
+++ SearchUX/src/root.js
@@ -2,8 +2,9 @@ let __CONFIG__ = require('__CONFIG__');
 
 if (__CONFIG__.thisENV === 'dev') {
   module.exports = require('./app.dev');
-} 
-else if (__CONFIG__.thisENV === 'test') 
+}
+/*
+if (__CONFIG__.thisENV === 'test') 
 {
   module.exports = require('./app.test');
-}
+}*/
