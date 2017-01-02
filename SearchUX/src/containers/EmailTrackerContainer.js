a1120c0abc1a4c635587ca38846c8a4fb5f5a25d
steven b <steveinatorx@gmail.com>
2017-01-01 17:14:39 -0800

    container for email tracker image

---
+++ SearchUX/src/containers/EmailTrackerContainer.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { EmailTrackerComponent } from '../components/EmailTrackerComponent';
+import { 
+  searchEmailView
+  } from '../actions/EmailTrackerComponentActions';
+
+export const EmailTrackerContainer = connect(
+  function mapStateToProps (state) {
+  return {
+    state: state
+    };
+  },
+
+  function mapDispatchToProps (dispatch) {
+    return {
+      searchEmailView: (data) => dispatch(searchEmailView(data))
+    };
+  }
+)(EmailTrackerComponent);
