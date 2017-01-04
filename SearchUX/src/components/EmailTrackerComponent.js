eeeab27d301bb330fcfe57fe04548818907612e7
steven b <steveinatorx@gmail.com>
2017-01-04 08:50:41 -0800

    proper image jsx

--- SearchUX/src/components/EmailTrackerComponent.js
+++ SearchUX/src/components/EmailTrackerComponent.js
@@ -3,14 +3,18 @@ import React from 'react';
 export class EmailTrackerComponent extends React.Component {
   constructor(props) {
     super(props);
+    console.log('hello');
   }
   componentWillReceiveProps (newProps) {
     console.log('CWRP new props', newProps);
    // console.log('CWRP res count', typeof newProps.state.get('resultsCount'));
   }
   render() {
+
+    let logoURL = require("../images/logo-1.png");
+
     return (
-      <img src="./images/logo-1.png" />
+      <img src={logoURL} />
     );
   }
 }
